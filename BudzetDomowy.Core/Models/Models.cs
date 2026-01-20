f1b6964edac302ae7bedece2d335d5af9ef7196d
Bzzyk <osako.kamil@gmail.com>
2026-01-20 18:41:38 +0100

    update models.cs, zeby obslugiwal composite

--- BudzetDomowy.Core/Models/Models.cs
+++ BudzetDomowy.Core/Models/Models.cs
@@ -1,32 +1,41 @@
-﻿namespace BudzetDomowy.Core.Models
+﻿using BudzetDomowy.Core.Patterns.CompositeMethod;
+
+namespace BudzetDomowy.Core.Models
 {
     public abstract class Transaction
     {
         public double Amount { get; set; }
         public DateTime Date { get; set; }
         public string Description { get; set; }
- 
+        public CategoryComponent Category { get; set; }
 
-        public Transaction(double amount, string description)
+
+        public Transaction(double amount, string description, CategoryComponent category)
         {
             Amount = amount;
             Date = DateTime.Now;
             Description = description;
+            Category = category;
         }
+
 
         public override string ToString()
         {
-            return $"{Date.ToShortDateString()} | {Description}: {Amount} PLN";
+            return $"{Date.ToShortDateString()} | [{Category?.Name}] {Description}: {Amount} PLN";
         }
+
     }
 
     public class Expense : Transaction
     {
-        public Expense(double amount, string description) : base(amount, description) { }
+        public Expense(double amount, string description, CategoryComponent category) 
+            : base(amount, description, category) { }
     }
 
     public class Income : Transaction
     {
-        public Income(double amount, string description) : base(amount, description) { }
+        public Income(double amount, string description, CategoryComponent category) 
+            : base(amount, description, category) { }
     }
+
 }
